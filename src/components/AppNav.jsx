fb845218a71daea09120368609087daae1d9ab09
thomasfregene <fregzythomas@gmail.com>
2024-10-24 05:11:00 +0100

    initial commit

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,12 @@
+import AppLayout from '../pages/AppLayout'
+import styles from './AppNav.module.css'
+
+function AppNav() {
+    return (
+        <nav className={styles.nav}>
+            App Navigation
+        </nav>
+    )
+}
+
+export default AppNav
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+function PageNotFound() {
+    return (
+        <div>
+            <PageNav />
+            <h1> Not Found :( </h1>
+        </div>
+    )
+}
+
+export default PageNotFound
